16ff9a30b64ffec262ce8cc742a58b61623e4fb2
Aleksandr Bogomolov <diadokrgsb@mail.com>
2025-02-09 17:29:18 +0300

    add prog

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " +
-                    Math.random());
+        System.out.print("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.print("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber; // Частное (с приведением к типу double)
+
+        // Вывод результатов
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + multiplication);
+        System.out.println("Частное: " + quotient);
+
+        //scanner.close();  // Закрываем Scanner
     }
 }
