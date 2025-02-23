106156b547962868f7a682b13905ab56f0862814
Aleksandr Bogomolov <diadokrgsb@mail.com>
2025-02-23 13:38:13 +0300

    add parser log-file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,26 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " +
-                    Math.random());
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+
+        while (true) {
+            System.out.print("Введите путь к файлу в формате 'C:\\...\\file.log': ");
+            String path = scanner.nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isFile = file.isFile();
+
+            if (!fileExists || !isFile) {
+                System.out.println("Указанный файл не существует или указанный путь является путём к папке, а не к файлу.");
+                continue;
+            }
+
+            count++;
+            System.out.println("Путь указан верно.");
+            System.out.println("Это файл номер " + count + ".");
+        }
     }
 }
